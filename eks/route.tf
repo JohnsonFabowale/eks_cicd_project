9bd43261f7cc55d1272a9cd2402ceffdccc810d2
Flourish <johnsonfabowale@gmail.com>
2023-08-04 16:49:43 +0100

    Cleaned repo

--- eks/route.tf
+++ eks/route.tf
@@ -2,14 +2,14 @@ resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
 
   route {
-      cidr_block        = "0.0.0.0/0"
-      nat_gateway_id    = aws_nat_gateway.nat-gateway.id
-    }
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat-gateway.id
+  }
 
-    tags = {
-      Name = "private-RT"
-    }
-  
+  tags = {
+    Name = "private-RT"
+  }
+
 }
 
 # resource "aws_route_table" "private" {
@@ -42,10 +42,10 @@ resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 
   route {
-      cidr_block                 = "0.0.0.0/0"
-      gateway_id                 = aws_internet_gateway.igw.id
-    }
-  
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
   tags = {
     Name = "public-RT"
   }
